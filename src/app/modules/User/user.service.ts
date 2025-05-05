60b9e0638c2d6250eccfc385c17ee203ba66b70e
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2025-05-05 12:37:40 +0600

    user status

--- src/app/modules/User/user.service.ts
+++ src/app/modules/User/user.service.ts
@@ -146,22 +146,22 @@ const getAllUsersFromDB = async (params: any, options: IPaginationOptions) => {
   };
 };
 
-// const changeProfileStatus = async (id: string, status: UserRole) => {
-//   const userData = await prisma.user.findUniqueOrThrow({
-//     where: {
-//       id,
-//     },
-//   });
+const changeProfileStatus = async (id: string, status: UserRole) => {
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  });
 
-//   const updateUserStatus = await prisma.user.update({
-//     where: {
-//       id,
-//     },
-//     data: status,
-//   });
+  const updateUserStatus = await prisma.user.update({
+    where: {
+      id,
+    },
+    data: status,
+  });
 
-//   return updateUserStatus;
-// };
+  return updateUserStatus;
+};
 
 // const getMyProfile = async (user: IAuthUser) => {
 //   const userInfo = await prisma.user.findUniqueOrThrow({
@@ -234,6 +234,7 @@ const getAllUsersFromDB = async (params: any, options: IPaginationOptions) => {
 export const UserServices = {
   // createAdmin,
   getAllUsersFromDB,
+  changeProfileStatus,
   // changeProfileStatus,
   // getMyProfile,
   // updateMyProfile,
