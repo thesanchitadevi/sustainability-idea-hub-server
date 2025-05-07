ebb6aa87d3895672bc2d07f2616a7f8b88959c71
Ranok Raihan <ranokraihan@gmail.com>
2025-05-07 11:14:26 +0600

    Refactor: Update comment and vote validation schemas to include body wrapper; enhance getCommentService to filter top-level comments and select user details

--- src/app/modules/comment/comment.validation.ts
+++ src/app/modules/comment/comment.validation.ts
@@ -1,13 +1,15 @@
 import { z } from "zod";
 
-export const createCommentSchema = z
-  .object({
-    ideaId: z.string({
-      required_error: "Idea ID is required",
-    }),
-    parentId: z.string().optional(),
-    comment: z.string({
-      required_error: "Comment is required",
-    }),
-  })
-  .strict();
+export const createCommentSchema = z.object({
+  body: z
+    .object({
+      ideaId: z.string({
+        required_error: "Idea ID is required",
+      }),
+      parentId: z.string().optional(),
+      comment: z.string({
+        required_error: "Comment is required",
+      }),
+    })
+    .strict(),
+});
